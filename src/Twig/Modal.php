c86bfe84474062a76ad2c3556d7e5438c355500b
João Ramajo <joaoramajo744@gmail.com>
2025-11-14 23:35:17 -0300

    chore(home): cria um componente modal para melhor organização do Card

--- src/Twig/Modal.php
+++ src/Twig/Modal.php
@@ -5,8 +5,8 @@ namespace App\Twig;
 use App\Entity\Book;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent('Card')]
-class Card
+#[AsTwigComponent('Modal')]
+class Modal
 {
     public Book $book;
 }
