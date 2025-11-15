f3a91115e9dbb0d4d1f2a4b8df87184e9a6a0868
João Ramajo <joaoramajo744@gmail.com>
2025-11-15 11:33:56 -0300

    feat(header): adiciona componente para header do site'

--- src/Controller/Web/PageController.php
+++ src/Controller/Web/PageController.php
@@ -3,14 +3,17 @@
 namespace App\Controller\Web;
 
 use App\Entity\Book;
+use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/home', name: 'home', methods: ['GET', 'HEAD'])]
+    #[Route('/', name: 'home', methods: ['GET', 'HEAD'])]
     public function home(EntityManagerInterface $entityManager): Response
     {
         $books = $entityManager->getRepository(Book::class)->all();
@@ -19,5 +22,19 @@ class PageController extends AbstractController
             'page_title' => 'Home Page',
             'books' => $books,
         ]);
+    }
+
+    #[Route('/books/search', name: 'books_search')]
+    public function search(Request $request, BookRepository $bookRepository): JsonResponse
+    {
+        $query = $request->query->get('search', '');
+
+        $books = $bookRepository->createQueryBuilder('b')
+            ->where('b.title LIKE :query')
+            ->setParameter('query', "%$query%")
+            ->getQuery()
+            ->getResult();
+
+        return $this->json($books, 200, ['groups' => 'book:list']);
     }
 }
